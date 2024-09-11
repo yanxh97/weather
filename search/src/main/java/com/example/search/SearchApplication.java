4c9e38f4f94090f80e75d5935b66e57c28678c3f
Xuehai Yan <xuyan@ucsd.edu>
2024-09-11 16:02:50 -0700

    Search Service discovers service by discoverClient. No need to hardcode addresses of school and details service. Also combine results of two responses.

--- search/src/main/java/com/example/search/SearchApplication.java
+++ search/src/main/java/com/example/search/SearchApplication.java
@@ -3,11 +3,17 @@ package com.example.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class SearchApplication {
+    @Bean
+    RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
--- search/src/main/java/com/example/search/controller/SearchController.java
+++ search/src/main/java/com/example/search/controller/SearchController.java
@@ -1,17 +1,40 @@
 package com.example.search.controller;
 
+import com.example.search.domain.GeneralResponse;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SearchController {
+    @Autowired
+    EurekaClient discoveryClient;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/weather/search")
     public ResponseEntity<?> getDetails() {
-        //TODO
-        return new ResponseEntity<>("this is search service", HttpStatus.OK);
+        //System.out.println(discoveryClient);
+
+        InstanceInfo detailsInstanceInfo = discoveryClient.getNextServerFromEureka("details", false);
+        String detailsURL = detailsInstanceInfo.getHomePageUrl();
+
+        GeneralResponse resDetails = restTemplate.getForObject(detailsURL + "/details/port", GeneralResponse.class);
+
+        InstanceInfo schoolInstanceInfo = discoveryClient.getNextServerFromEureka("school", false);
+        String schoolURL = schoolInstanceInfo.getHomePageUrl();
+
+        String resSchool = restTemplate.getForObject(schoolURL + "/school", String.class);
+        resDetails.setData(resSchool);
+
+        return new ResponseEntity<>(resDetails, HttpStatus.OK);
     }
 }
