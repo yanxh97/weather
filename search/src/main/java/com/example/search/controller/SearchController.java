5484c372ef8c271b8e89dea76ae20666dcd82c22
Xuehai Yan <xuyan@ucsd.edu>
2024-09-12 11:29:35 -0700

    Enable timeout hystrix.
    TODO
    circuit breaker hystrix
    keystore

--- search/src/main/java/com/example/search/controller/SearchController.java
+++ search/src/main/java/com/example/search/controller/SearchController.java
@@ -1,18 +1,10 @@
 package com.example.search.controller;
 
-import com.example.search.domain.GeneralResponse;
 import com.example.search.service.SearchService;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.EurekaClient;
-import io.micrometer.core.instrument.search.Search;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SearchController {
--- search/src/main/java/com/example/search/service/SearchServiceImpl.java
+++ search/src/main/java/com/example/search/service/SearchServiceImpl.java
@@ -4,8 +4,11 @@ import com.example.search.domain.GeneralResponse;
 import com.example.search.domain.StudentDTO;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import io.micrometer.core.instrument.search.Search;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Service
+@EnableHystrix
 public class SearchServiceImpl implements SearchService{
     private final EurekaClient discoveryClient;
     private final RestTemplate restTemplate;
@@ -28,6 +32,9 @@ public class SearchServiceImpl implements SearchService{
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "failureResponse",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+    })
     public ResponseEntity<?> getDetails() {
 
 //        InstanceInfo detailsInstanceInfo = discoveryClient.getNextServerFromEureka("details", false);
@@ -63,5 +70,9 @@ public class SearchServiceImpl implements SearchService{
         mergedRes.setData((StudentDTO) futures.get(1).join());
         return new ResponseEntity<>(mergedRes, HttpStatus.OK);
 
+    }
+
+    public ResponseEntity<?> failureResponse(){
+        return new ResponseEntity<String>("Request fails due to time out", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
